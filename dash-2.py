f4a7ae392c5d858b16d99f5b14aedf9a24eb5bbc
ahb <ahb@cisco.com>
2023-03-08 15:02:27 -0800

    added comments

--- dash-2.py
+++ dash-2.py
@@ -1,3 +1,20 @@
+"""
+Written by Andrew Bond
+
+Python Developer | Network Designer | Superyacht Builder (M/Y Eris)
+
+# Copyright (c) 2023, ANDREW BOND. All rights reserved.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LinkedIn: https://www.linkedin.com/in/ahbond/
+SJSU: https://www.sjsu.edu/people/andrew.bond/
+Email: abptlm@gmail.com
+"""
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
